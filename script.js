14ab2a5cb10dba6c7f04821998d7d6f8923419cf
Shibu56 <99981074+Shibu56@users.noreply.github.com>
2022-09-21 13:58:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+             var amt = document.getElementById("principal").value;
+             var int=document.getElementById("rate").value;
+             var yr=document.getElementById("years").value;
+             var famount=amt*yr*int/100;
+             var year=new Date().getFullYear()+parseInt(yr);
+             document.getElementById("result").innerHTML="If you deposit "+amt+",\<br\>at an interest rate of "+int+"%\<br\>You will receive an amount of "+famount+",\<br\>in the year "+yr+"\<br\>"
     
 }
         
